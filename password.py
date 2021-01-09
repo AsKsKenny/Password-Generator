dad698a606836f8e0b6573631e0eac03cfe81405
AsKsKenny <34576273+AsKsKenny@users.noreply.github.com>
2021-01-09 17:13:15 +0100

    Update password.py

--- password.py
+++ password.py
@@ -4,3 +4,4 @@ import string
 chars = string.digits + string.ascii_letters + string.punctuation
 print((len)(chars))
 print(''.join(secrets.choice(chars) for _ in range (40)))
+# You can edit the numbers of digits for your password by changing the number 40 to what ever number you like.
