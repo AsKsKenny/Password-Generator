dedd296d7b861e31244b3fa5b40b155e54366e83
AsKsKenny <34576273+AsKsKenny@users.noreply.github.com>
2021-01-23 20:43:38 +0100

    Update password_generator_gui.py

--- password_generator_gui.py
+++ password_generator_gui.py
@@ -1,6 +1,6 @@
 import pyperclip
 import tkinter as tk
-from random import randint
+from tkinter import Text, Tk
 import secrets
 import string
 
@@ -17,9 +17,9 @@ def test_button_click():
         label_val.set(var)
         pyperclip.copy(clipboard_var)
 
-my_button = tk.Button(win, text='Generate new 256-Bit Password', command=test_button_click)
-my_button.grid(column=0, row=0)
+my_button = tk.Button(win, width=25, text='Generate new 256-Bit Password', command=test_button_click)
+my_button.grid(column=0, row=2)
 label_val = tk.StringVar()
-my_Label = tk.Label(win, textvariable=label_val)
-my_Label.grid(column=1, row=0)
+my_Label = tk.Entry(win, width=50, textvariable=label_val)
+my_Label.grid(column=2, row=2, ipady=5)
 win.mainloop()
